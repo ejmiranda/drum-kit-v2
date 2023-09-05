203f5254eda24f1adcf90fcdcf56543ee60a2291
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-05 21:42:46 +0000

    Add and play audio files

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const keys = document.querySelectorAll('.key');
 
 window.addEventListener('keydown', (event) => {
-  keys.forEach((key) => {
+  keys.forEach((key) => { 
     if (key.getAttribute('id') === event.key.toLowerCase()) {
       key.classList.add('down');
+      const audio = new Audio(getMediaPath(key.getAttribute('id')));
+      audio.play();
     }
   })
 });
 
+function getMediaPath(key) {
+  switch(key) {
+    case 'a': return 'media/mario.wav';
+    case 's': return 'media/luigi.wav';
+    case 'd': return 'media/peach.wav';
+    case 'f': return 'media/toad.wav';
+    case 'g': return 'media/yoshi.wav';
+    case 'h': return 'media/wario.wav';
+    case 'j': return 'media/bowser.wav';
+    case 'k': return 'media/boo.wav';
+    case 'l': return 'media/game-over.wav';
+  }
+}
